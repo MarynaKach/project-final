cf5b384eedcca872ecb5a6f15311ff7527361809
Maryna Kachalova <masha.kach80@gmail.com>
2023-05-12 08:50:59 +0300

    added saveTaskTag and deleteTagByTaskId methods in TaskRepository, test it via swagger

--- src/main/java/com/javarush/jira/bugtracking/TaskService.java
+++ src/main/java/com/javarush/jira/bugtracking/TaskService.java
@@ -4,11 +4,17 @@ import com.javarush.jira.bugtracking.internal.mapper.TaskMapper;
 import com.javarush.jira.bugtracking.internal.model.Task;
 import com.javarush.jira.bugtracking.internal.repository.TaskRepository;
 import com.javarush.jira.bugtracking.to.TaskTo;
+import com.javarush.jira.common.error.NotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
 @Service
+@RestController
+@RequestMapping("/api")
 public class TaskService extends BugtrackingService<Task, TaskTo, TaskRepository> {
     public TaskService(TaskRepository repository, TaskMapper mapper) {
         super(repository, mapper);
@@ -16,5 +22,20 @@ public class TaskService extends BugtrackingService<Task, TaskTo, TaskRepository
 
     public List<TaskTo> getAll() {
         return mapper.toToList(repository.getAll());
+    }
+    @PutMapping
+
+    public void saveTaskTag(Long taskId, String tag){
+        if(repository.existsById(taskId))
+            repository.saveTaskTag(taskId, tag);
+        else
+            throw new NotFoundException("No task with such "+taskId + " found");
+    }
+    @DeleteMapping
+    public void deleteTaskTag(Long taskId){
+        if(repository.existsById(taskId))
+            repository.deleteTagByTaskId(taskId);
+        else
+            throw new NotFoundException("No task with such "+taskId + " found");
     }
 }
--- src/main/java/com/javarush/jira/bugtracking/internal/repository/TaskRepository.java
+++ src/main/java/com/javarush/jira/bugtracking/internal/repository/TaskRepository.java
@@ -2,7 +2,9 @@ package com.javarush.jira.bugtracking.internal.repository;
 
 import com.javarush.jira.bugtracking.internal.model.Task;
 import com.javarush.jira.common.BaseRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -11,4 +13,14 @@ import java.util.List;
 public interface TaskRepository extends BaseRepository<Task> {
     @Query("SELECT t FROM Task t LEFT JOIN FETCH t.project LEFT JOIN FETCH t.sprint LEFT JOIN FETCH t.activities")
     List<Task> getAll();
+
+    @Transactional
+    @Modifying
+    @Query(value = "INSERT INTO  task_tag (task_id, tag) VALUES (:taskId, :tag)", nativeQuery = true)
+    void saveTaskTag(@Param("taskId") Long taskId, @Param("tag") String tag);
+
+    @Transactional
+    @Modifying
+    @Query(value = "DELETE FROM task_tag WHERE task_id = :taskId", nativeQuery = true)
+    void deleteTagByTaskId(@Param("taskId") Long taskId);
 }
